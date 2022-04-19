428563f17cd7e1f0071136934a3c110c58326b85
Lourães <peterlouraes@gmail.com>
2022-04-19 22:06:42 +0100

    Removed E content that was from F and added to F.

--- Introdutório/Python/E.py
+++
@@ -1,4 +0,0 @@
-N, Ni, M, Mi = int(input()), map(int, input().split()), int(input()), map(int, input().split())
-
-# resolução resumida para dia 20/04 - o problema consiste em encontrar uma maneira de somar todos os elementos da lista entre si, isto é, os pares da lista sem repetir elementos.
-# depois do problema resolvido, basta verificar se esta soma é par ou impar e adicionar a uma das duas variáveis da saída de dados, o parsoma ou o imparsoma
