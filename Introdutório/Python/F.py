6ea7285f745720f0c7161f370d311cac352fa78d
Lourães <87422391+Louraes202@users.noreply.github.com>
2022-04-20 19:19:17 +0100

    Add files via upload

--- Introdutório/Python/F.py
+++ Introdutório/Python/F.py
@@ -1,4 +1,18 @@
-N, Ni, M, Mi = int(input()), map(int, input().split()), int(input()), map(int, input().split())
 
-# resolução resumida para dia 20/04 - o problema consiste em encontrar uma maneira de somar todos os elementos da lista entre si, isto é, os pares da lista sem repetir elementos.
-# depois do problema resolvido, basta verificar se esta soma é par ou impar e adicionar a uma das duas variáveis da saída de dados, o parsoma ou o imparsoma
+# N, Ni, M, Mi = int(input()), map(int, input().split()), int(input()), map(int, input().split())
+N, Ni, M, Mi = int(input()), list(map(int, input().split())), int(input()), list(map(int, input().split()))
+
+pairsN = [(Ni[i],Ni[j]) for i in range(len(Ni)) for j in range(i+1, len(Ni))]
+pairsM = [(Ni[i],Ni[j]) for i in range(len(Ni)) for j in range(i+1, len(Ni))]
+pairs = pairsN + pairsM
+somapares, somaimpares = 0, 0
+
+print(pairsM)
+
+# for l in pairs:
+
+
+
+
+
+print(somapares, somaimpares)
